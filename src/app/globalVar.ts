515dcd0b0d1c4bc629267c8e6d3c5ee4f2c515e9
Mayur Gajapure <mayur.gajapure@vginfo.com>
2018-02-01 16:04:19 +0530

    vc

--- src/app/globalVar.ts
+++ src/app/globalVar.ts
@@ -4,3 +4,4 @@ export const web_url='http://192.168.5.69:8090/api/';
 export const web_url2='http://192.168.5.69:8090/api/';
 export const web_url3='http://192.168.5.69:8090/api/';
 export const web_url4='http://192.168.5.69:8090/api/';
+export const web_url5='http://192.168.5.69:8090/api/';
